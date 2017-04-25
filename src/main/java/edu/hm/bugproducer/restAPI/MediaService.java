0e747712178d6705d05d7950087d5e7899fbc2e7
Tom Maier <maier14@hm.edu>
2017-04-25 11:48:05 +0200

    add getBooks() and getDiscs()

--- src/main/java/edu/hm/bugproducer/restAPI/MediaService.java
+++ src/main/java/edu/hm/bugproducer/restAPI/MediaService.java
@@ -1,17 +1,25 @@
 package edu.hm.bugproducer.restAPI;
+
 import edu.hm.bugproducer.models.Book;
 import edu.hm.bugproducer.models.Disc;
 import edu.hm.bugproducer.models.Medium;
+
+import java.util.List;
 
 /**
  * Created by Johannes Arzt on 25.04.17.
  */
 public interface MediaService {
     public MediaServiceResult addBook(Book book);
+
     public MediaServiceResult addDisc(Disc disc);
-    public Medium[] getBooks();
-    public Medium[] getDisc();
+
+    public List<Book> getBooks();
+
+    public List<Disc> getDiscs();
+
     public MediaServiceResult updateBook(Book book);
+
     public MediaServiceResult updateDisc(Disc disc);
 
 }
