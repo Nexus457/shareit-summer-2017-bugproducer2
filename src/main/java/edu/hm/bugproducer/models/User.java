3629709164130ac4fc9bc582a282760cdd84cd36
Tom Maier <maier14@hm.edu>
2017-05-02 10:56:32 +0200

    add createCopy(Medium, user)

--- src/main/java/edu/hm/bugproducer/models/User.java
+++ src/main/java/edu/hm/bugproducer/models/User.java
@@ -5,6 +5,11 @@ public class User {
     private String userName;
 
     public User() {
+        userName = "No Name";
+    }
+
+    public User(String userName) {
+        this.userName = userName;
     }
 
     public String getUserName() {
