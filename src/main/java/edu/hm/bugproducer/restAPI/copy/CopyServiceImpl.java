48aab6bf052cd5c65b944b8feb252af158ae2877
Johannes Arzt <Johannes.arzt@gmx.net>
2017-05-05 13:12:50 +0200

    bugfix !!!!!!!!!!!

--- src/main/java/edu/hm/bugproducer/restAPI/copy/CopyServiceImpl.java
+++ src/main/java/edu/hm/bugproducer/restAPI/copy/CopyServiceImpl.java
@@ -6,14 +6,13 @@ import edu.hm.bugproducer.restAPI.MediaServiceResult;
 import edu.hm.bugproducer.restAPI.media.MediaServiceImpl;
 import javafx.util.Pair;
 import org.apache.commons.validator.routines.checkdigit.EAN13CheckDigit;
-import org.jvnet.hk2.internal.Collector;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 
 import static edu.hm.bugproducer.restAPI.MediaServiceResult.*;
-import static edu.hm.bugproducer.restAPI.media.MediaServiceImpl.discs;
+import static edu.hm.bugproducer.restAPI.media.MediaServiceImpl.books;
 
 public class CopyServiceImpl implements CopyService {
 
@@ -28,8 +27,9 @@ public class CopyServiceImpl implements CopyService {
 
     @Override
     public MediaServiceResult addCopy(String username, String code) {
+        boolean noUserFound=false;
 
-        MediaServiceResult mediaServiceResult = MSR_INTERNAL_SERVER_ERROR;
+       // MediaServiceResult mediaServiceResult = MSR_INTERNAL_SERVER_ERROR;
 
         if (EAN13CheckDigit.EAN13_CHECK_DIGIT.isValid(code)) {
 
@@ -51,78 +51,94 @@ public class CopyServiceImpl implements CopyService {
                     disclfnr=copyList.size()+1;
 
                     if (users.isEmpty()) {
-                        User user2 = new User(username);
-                        users.add(user2);
-                        copies.add(new Copy(d, user2, disclfnr));
-                        disclfnr++;
-                        mediaServiceResult = MSR_OK;
+                        return addUserAndAddDiscCopy(username, d);
                     }
 
                     for (User user1 : users) {
                         if (user1.getUserName().equals(username)) {
-                            copies.add(new Copy(d, user1, disclfnr));
-                            disclfnr++;
-                            mediaServiceResult = MSR_OK;
-                            System.out.println("#noduplicate");
+                            return addDiscCopy(d, user1);
                         } else {
-                            User user2 = new User(username);
-                            users.add(user2);
-                            copies.add(new Copy(d, user2, disclfnr));
-                            disclfnr++;
-                            mediaServiceResult = MSR_OK;
+                            noUserFound=true;
                         }
                     }
+                    if(noUserFound){
+                        return addUserAndAddDiscCopy(username, d);
+                    }
                 } else {
-                    mediaServiceResult = MSR_BAD_REQUEST;
+                    return MSR_BAD_REQUEST;
                 }
             }
         } else if (Isbn.isValid(code)) {
-            for (Book b : MediaServiceImpl.books) {
-                if (b.getIsbn().equals(code)) {
-                    List<Book> copyList = copies
-                            .stream()
-                            .map(book -> {
-                                if (book.getMedium() instanceof Book) {
-                                    return (Book) book.getMedium();
-                                } else {
-                                    return null;
-                                }
-                            })
-                            .filter(Objects::nonNull)
-                            .filter(book -> book.getIsbn().equals(code))
-                            .collect(Collectors.toList());
-                    booklfnr=copyList.size()+1;
-
-                    if (users.isEmpty()) {
-                        User user2 = new User(username);
-                        users.add(user2);
-                        copies.add(new Copy(b, user2, booklfnr));
-                        booklfnr++;
-                        mediaServiceResult = MSR_OK;
-                    } else {
-                        for (User user1 : users) {
-                            if (user1.getUserName().equals(username)) {
-                                copies.add(new Copy(b, user1, booklfnr));
-                                booklfnr++;
-                                mediaServiceResult = MSR_OK;
-                            } else {
-                                User user2 = new User(username);
-                                users.add(user2);
-                                copies.add(new Copy(b, user2, booklfnr));
-                                booklfnr++;
-                                mediaServiceResult = MSR_OK;
-                            }
-                        }
-                    }
-                } else {
-                    mediaServiceResult = MSR_BAD_REQUEST;
-                }
-            }
+            MediaServiceResult x = checkBook(username, code, noUserFound);
+            if (x != null) return x;
         } else {
-            mediaServiceResult = MSR_NO_CONTENT;
+            return MSR_NO_CONTENT;
         }
 
-        return mediaServiceResult;
+        return MSR_INTERNAL_SERVER_ERROR;
+    }
+
+    private MediaServiceResult addDiscCopy(Disc d, User user1) {
+        copies.add(new Copy(d, user1, disclfnr));
+        disclfnr++;
+        return MSR_OK;
+    }
+
+    private MediaServiceResult addUserAndAddDiscCopy(String username, Disc d) {
+        User user2 = new User(username);
+        users.add(user2);
+        return addDiscCopy(d, user2);
+    }
+
+    private MediaServiceResult checkBook(String username, String code, boolean noUserFound) {
+        int counter = 0;
+        for (Book b : MediaServiceImpl.books) {
+            if (b.getIsbn().equals(code)) {
+                List<Book> copyList = copies
+                        .stream()
+                        .map(book -> {
+                            if (book.getMedium() instanceof Book) {
+                                return (Book) book.getMedium();
+                            } else {
+                                return null;
+                            }
+                        })
+                        .filter(Objects::nonNull)
+                        .filter(book -> book.getIsbn().equals(code))
+                        .collect(Collectors.toList());
+                booklfnr=copyList.size()+1;
+
+                if (users.isEmpty()) {
+                    return createUserAndAddBookCopy(username, b);
+                } else {
+                    for (User user1 : users) {
+                        if (user1.getUserName().equals(username)) {
+                            return addCopy(b, user1);
+                        } else {
+                       noUserFound=true;
+                        }
+                    }if(noUserFound) {
+                        return createUserAndAddBookCopy(username, b);
+                    }
+                }
+            } else {
+                if(counter == books.size()) return MSR_BAD_REQUEST;
+            }
+            counter++;
+        }
+        return null;
+    }
+
+    private MediaServiceResult addCopy(Book b, User user1) {
+        copies.add(new Copy(b, user1, booklfnr));
+        booklfnr++;
+        return MSR_OK;
+    }
+
+    private MediaServiceResult createUserAndAddBookCopy(String username, Book b) {
+        User user2 = new User(username);
+        users.add(user2);
+        return addCopy(b, user2);
     }
 
 
