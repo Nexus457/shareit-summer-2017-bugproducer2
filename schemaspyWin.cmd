03e76d457c2a0d1544aa4183b4cc976023b33a53
Johdeath <johannes.arzt@gmx.net>
2017-06-11 14:28:59 +0200

    add dbStarter and schemaspy Windows skript

---
+++ schemaspyWin.cmd
@@ -0,0 +1,2 @@
+java -jar ./lib/schemaSpy_5.0.0.jar -t hsqldb -db shareit-db -host localhost -u sa -noschema -dp ./lib/hsqldb-2.3.0.jar -o db-schema
+pause
---
+++ startdbWin.cmd
@@ -0,0 +1,3 @@
+java -cp ./lib/hsqldb-2.3.0.jar org.hsqldb.server.Server --database.0 file:./shareit-db --dbname.0 shareit-db
+
+pause
